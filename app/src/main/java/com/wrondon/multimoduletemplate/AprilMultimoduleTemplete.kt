6a9f4c180074113dbde1f538ecfe8b99605c8682
William Rondon <wfrondon2010@gmail.com>
2023-08-10 09:58:29 -0400

    Changing name to include august and upgrading some versions

--- app/src/main/java/com/wrondon/multimoduletemplate/AprilMultimoduleTemplete.kt
+++ app/src/main/java/com/wrondon/multimoduletemplate/AprilMultimoduleTemplete.kt
@@ -20,4 +20,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AprilMultimoduleTemplete : Application()
+class AugustMultimoduleTemplete : Application()
